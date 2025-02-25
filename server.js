321dc84e0d79090de91efbae83cfcbba825ca011
Nathan94600 <nathan94nahtan@gmail.com>
2025-02-25 19:41:24 +0100

    Correction de problème dans le fichier server.js, création d'un dossier styles avec tous le css, réorganisation du html

--- server.js
+++ server.js
@@ -1,62 +1,25 @@
 const { createServer } = require("http"), { readFile } = require("fs");
 
 createServer((req, res) => {
+	const { pathname: url } = new URL(req.url, "http://localhost:8080");
+
 	switch (req.method) {
 		case "GET":
-			switch (req.url) {
-				case "/":
-					readFile("./pages/index.html", (err, data) => {
-						if (err) throw err;
-						
-						res.writeHead(200, { "content-type": "text/html" }).write(data)
-						res.end()
-					})
-					break;
-				case "/connexion":
-					readFile("./pages/connexion.html", (err, data) => {
-						if (err) throw err;
-						
-						res.writeHead(200, { "content-type": "text/html" }).write(data)
-						res.end()
-					})
-					break;
-				case "/inscription":
-					readFile("./pages/inscription.html", (err, data) => {
-						if (err) throw err;
-						
-						res.writeHead(200, { "content-type": "text/html" }).write(data)
-						res.end()
-					})
-					break;
-				case "/produits":
-					readFile("./pages/produits.html", (err, data) => {
-						if (err) throw err;
-						
-						res.writeHead(200, { "content-type": "text/html" }).write(data)
-						res.end()
-					})
-					break;
-				case "/style.css":
-					readFile("./style.css", (err, data) => {
-						if (err) throw err;
-						
-						res.writeHead(200, { "content-type": "text/css" }).write(data)
-						res.end()
-					})
-					break;
-				default:
-					if (req.url.startsWith("/images/")) readFile(`.${req.url}`, (err, data) => {
-						if (err) throw err;
-						
-						res.writeHead(200, { "content-type": `image/${req.url.split(".").at(-1)}` }).write(data)
-						res.end()
-					})
-					else res.writeHead(404, "Not found").end()
-					break;
-			}
+			if (url.startsWith("/images/")) readFile(`.${url}`, (err, data) => {
+				if (err) res.writeHead(404, "Not found").end();
+				else res.writeHead(200, { "content-type": `image/${url.split(".").at(-1)}` }).end(data);
+			});
+			else if (url.startsWith("/styles/")) readFile(`.${url}`, (err, data) => {
+				if (err) res.writeHead(404, "Not found").end();
+				else res.writeHead(200, { "content-type": `text/css` }).end(data);
+			});
+			else readFile(`./pages${url == "/" ? "/index" : url}.html`, (err, data) => {
+				if (err) res.writeHead(404, "Not found").end();
+				else res.writeHead(200, { "content-type": `text/html` }).end(data);
+			});
 			break;
 		default:
-			res.writeHead(404, "Not found").end()
+			res.writeHead(404, "Not found").end();
 			break;
-	}
-	}).listen(8080, () => console.log("http://localhost:8080")) 
+	};
+}).listen(8080, () => console.log("http://localhost:8080"));
