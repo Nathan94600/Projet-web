4d4310ad3fdb161914b1597b481dcd96d563b6c2
Nathan94600 <nathan94nahtan@gmail.com>
2025-03-24 13:50:48 +0100

    Création des tables sql `products` et `stocks`

--- server.js
+++ server.js
@@ -9,7 +9,26 @@ supportedEncodings = ["*", "br", "deflate", "gzip"],
 db = new Database("database.db", err => {
 	if (err) console.log("Erreur lors de la connexion à la base de données: ", err);
 	else console.log("Connexion à la base de données réussie");
-});
+}),
+colors = {
+	Noir: 2**2,
+	Jaune: 2**6,
+	Rose: 2**7,
+	Marron: 2**9,
+	Orange: 2**5,
+	Violet: 2**3,
+	Gris: 2*10,
+	Rouge: 2**0,
+	Bleu: 2**1,
+	Vert: 2**4,
+	Blanc: 2**8
+},
+sexes = {
+	Homme: "h",
+	Femme: "f",
+	Enfant: "e",
+	Mixte: "m",
+};
 
 /**
  * @param { string } password 
@@ -104,102 +123,108 @@ function getPage(pageURL, params = {}) {
 	});
 };
 
-db.exec("CREATE TABLE IF NOT EXISTS users (id TEXT PRIMARY KEY, email TEXT, username TEXT, password TEXT, password_salt TEXT)", err => {
-	if (err) console.log("Erreur lors de la création de la table users: ", err);
-	else {
-		createServer((req, res) => {
-			const { pathname: url, searchParams } = new URL(req.url, "http://localhost:8080"), errorMessage = searchParams.get("error"), userToken = new URLSearchParams(req.headers.cookie || "").get("token");
+db.exec("CREATE TABLE IF NOT EXISTS products (id CHAR(36) NOT NULL PRIMARY KEY, supplierId VARCHAR(20) NOT NULL, name VARCHAR(50) NOT NULL, price INT NOT NULL, promoPrice INT, type CHAR(1) NOT NULL, colors INT NOT NULL, CHECK (type IN ('h', 'f', 'e', 'm')))", err => {
+	if (err) console.log("Erreur lors de la création de la table products: ", err);
+	else db.exec("CREATE TABLE IF NOT EXISTS stocks (id CHAR(36) NOT NULL PRIMARY KEY, productId CHAR(36) NOT NULL, quantity INT NOT NULL, size INT NOT NULL, FOREIGN KEY (productId) REFERENCES products(id) ON DELETE CASCADE)", err => {
+		if (err) console.log("Erreur lors de la création de la table stocks: ", err);
+		else db.exec("CREATE TABLE IF NOT EXISTS users (id CHAR(36) NOT NULL PRIMARY KEY, email TEXT NOT NULL, username VARCHAR(20) NOT NULL, password CHAR(128) NOT NULL, password_salt CHAR(256) NOT NULL)", err => {
+			if (err) console.log("Erreur lors de la création de la table users: ", err);
+			else {
+				createServer((req, res) => {
+					const { pathname: url, searchParams } = new URL(req.url, "http://localhost:8080"), errorMessage = searchParams.get("error"), userToken = new URLSearchParams(req.headers.cookie || "").get("token");
 
-			compressData(req.headers["accept-encoding"], "");
-		
-			switch (req.method) {
-				case "GET":
-					if (url.startsWith("/images/")) readFile(`.${url}`, (err, data) => {
-						if (err) res.writeHead(404, "Not found").end();
-						else res.writeHead(200, { "content-type": `image/${url.endsWith(".svg") ? "svg+xml" : url.split(".").at(-1)}` }).end(data);
-					});
-					else if (url.startsWith("/styles/")) readFile(`.${url}`, (err, data) => {
-						if (err) res.writeHead(404, "Not found").end();
-						else compressData(req.headers["accept-encoding"], data).then(compression => res.writeHead(200, { "content-type": `text/css`, "content-encoding": compression.encoding }).end(compression.data));
-					});
-					else if ((url == "/inscription" || url == "/connexion") && userToken) res.writeHead(302, { location: "/" }).end();
-					else getPage(url, { error: errorMessage ? `<p id="error">${errorMessage}</p>` : "", accountText: userToken ? "Mon compte" : "Se connecter" }).then(
-						data => compressData(req.headers["accept-encoding"], data).then(compression => res.writeHead(200, { "content-type": `text/html`, "content-encoding": compression.encoding }).end(compression.data)),
-						() => res.writeHead(404, "Not found").end()
-					);
-					break;
-				case "POST":
-					if (url == "/connexion") {
-						let body = "";
+					compressData(req.headers["accept-encoding"], "");
 
-						req.on("data", chunk => body += chunk).on("end", () => {
-							const params = new URLSearchParams(body), email = params.get("email"), password = params.get("password");
-		
-							let errorMessage = "";
-		
-							if (!email) errorMessage = "Vous devez mettre un email";
-							else if (!password) errorMessage = "Vous devez mettre un mot de passe";
-							else if (password.length > 20 || password.length < 8) errorMessage = "Votre mot de passe doit contenir entre 8 et 20 caractères";
-		
-							if (errorMessage) res.writeHead(302, { location: `/inscription?error=${encodeURIComponent(errorMessage)}` }).end();
-							else db.get("SELECT * FROM users WHERE email = ?", email, (err, row) => {								
-								if (err) {
-									console.log("Erreur lors de la vérication de l'email: ", err);
-	
-									res.writeHead(302, { location: `/connexion?error=${encodeURIComponent("Erreur lors de la vérification de l'email")}` }).end();
-								} else if (!row) res.writeHead(302, { location: `/connexion?error=${encodeURIComponent("Aucun compte n'est associé à cet email")}` }).end();
-								else if (row.password != securePassword(password, row.password_salt).password) res.writeHead(302, { location: `/connexion?error=${encodeURIComponent("Mot de passe incorrect")}` }).end();
-								else res.writeHead(302, { location: "/", "set-cookie": `token=${randomBytes(64).toString('hex')}.${row.id};` }).end();
+					switch (req.method) {
+						case "GET":
+							if (url.startsWith("/images/")) readFile(`.${url}`, (err, data) => {
+								if (err) res.writeHead(404, "Not found").end();
+								else res.writeHead(200, { "content-type": `image/${url.endsWith(".svg") ? "svg+xml" : url.split(".").at(-1)}` }).end(data);
 							});
-						});
-					} else if (url == "/inscription") {						
-						let body = "";
+							else if (url.startsWith("/styles/")) readFile(`.${url}`, (err, data) => {
+								if (err) res.writeHead(404, "Not found").end();
+								else compressData(req.headers["accept-encoding"], data).then(compression => res.writeHead(200, { "content-type": `text/css`, "content-encoding": compression.encoding }).end(compression.data));
+							});
+							else if ((url == "/inscription" || url == "/connexion") && userToken) res.writeHead(302, { location: "/" }).end();
+							else getPage(url, { error: errorMessage ? `<p id="error">${errorMessage}</p>` : "", accountText: userToken ? "Mon compte" : "Se connecter" }).then(
+								data => compressData(req.headers["accept-encoding"], data).then(compression => res.writeHead(200, { "content-type": `text/html`, "content-encoding": compression.encoding }).end(compression.data)),
+								() => res.writeHead(404, "Not found").end()
+							);
+							break;
+						case "POST":
+							if (url == "/connexion") {
+								let body = "";
 
-						req.on("data", chunk => body += chunk).on("end", () => {
-							const params = new URLSearchParams(body), email = params.get("email"), username = params.get("username"), password = params.get("password"), password2 = params.get("password2");
-		
-							let errorMessage = "";
-		
-							if (!email) errorMessage = "Vous devez mettre un email";
-							else if (!username) errorMessage = "Vous devez mettre un nom d'utilisateur";
-							else if (!password) errorMessage = "Vous devez mettre un mot de passe";
-							else if (!password2) errorMessage = "Vous devez confirmer votre mot de passe";
-							else if (username.length > 20 || username.length < 3) errorMessage = "Votre nom d'utilisateur doit contenir entre 3 et 20 caractères";
-							else if (password.length > 20 || password.length < 8 || password2.length > 20 || password2.length < 8) errorMessage = "Votre mot de passe doit contenir entre 8 et 20 caractères";
-							else if (password != password2) errorMessage = "Les mots de passe ne correspondent pas";
-		
-							if (errorMessage) res.writeHead(302, { location: `/inscription?error=${encodeURIComponent(errorMessage)}` }).end();
-							else {
-								const { password: encryptedPassword, passwordSalt } = securePassword(password), userId = randomUUID({ disableEntropyCache: true });
+								req.on("data", chunk => body += chunk).on("end", () => {
+									const params = new URLSearchParams(body), email = params.get("email"), password = params.get("password");
 
-								db.get("SELECT * FROM users WHERE email = ?", email, (err, row) => {
-									if (err) {
-										console.log("Erreur lors de la vérication de l'email: ", err);
-	
-										res.writeHead(302, { location: `/inscription?error=${encodeURIComponent("Erreur lors de la vérification de l'email")}` }).end();
-									} else if (row) res.writeHead(302, { location: `/inscription?error=${encodeURIComponent("Cet email est déjà utilisé")}` }).end();
-									else db.exec(`INSERT INTO users (id, email, username, password, password_salt) VALUES (
-										"${userId}",
-										"${email}",
-										"${username}",
-										"${encryptedPassword}",
-										"${passwordSalt}"
-									)`, err => {
+									let errorMessage = "";
+
+									if (!email) errorMessage = "Vous devez mettre un email";
+									else if (!password) errorMessage = "Vous devez mettre un mot de passe";
+									else if (password.length > 20 || password.length < 8) errorMessage = "Votre mot de passe doit contenir entre 8 et 20 caractères";
+
+									if (errorMessage) res.writeHead(302, { location: `/inscription?error=${encodeURIComponent(errorMessage)}` }).end();
+									else db.get("SELECT * FROM users WHERE email = ?", email, (err, row) => {								
 										if (err) {
-											console.log("Erreur lors de la création du compte : ", err);
-	
-											res.writeHead(302, { location: `/inscription?error=${encodeURIComponent("Erreur lors de la création du compte ")}` }).end();
-										} else res.writeHead(302, { location: "/", "set-cookie": `token=${randomBytes(64).toString('hex')}.${userId};` }).end();
+											console.log("Erreur lors de la vérication de l'email: ", err);
+
+											res.writeHead(302, { location: `/connexion?error=${encodeURIComponent("Erreur lors de la vérification de l'email")}` }).end();
+										} else if (!row) res.writeHead(302, { location: `/connexion?error=${encodeURIComponent("Aucun compte n'est associé à cet email")}` }).end();
+										else if (row.password != securePassword(password, row.password_salt).password) res.writeHead(302, { location: `/connexion?error=${encodeURIComponent("Mot de passe incorrect")}` }).end();
+										else res.writeHead(302, { location: "/", "set-cookie": `token=${randomBytes(64).toString('hex')}.${row.id};` }).end();
 									});
 								});
+							} else if (url == "/inscription") {						
+								let body = "";
+
+								req.on("data", chunk => body += chunk).on("end", () => {
+									const params = new URLSearchParams(body), email = params.get("email"), username = params.get("username"), password = params.get("password"), password2 = params.get("password2");
+
+									let errorMessage = "";
+
+									if (!email) errorMessage = "Vous devez mettre un email";
+									else if (!username) errorMessage = "Vous devez mettre un nom d'utilisateur";
+									else if (!password) errorMessage = "Vous devez mettre un mot de passe";
+									else if (!password2) errorMessage = "Vous devez confirmer votre mot de passe";
+									else if (username.length > 20 || username.length < 3) errorMessage = "Votre nom d'utilisateur doit contenir entre 3 et 20 caractères";
+									else if (password.length > 20 || password.length < 8 || password2.length > 20 || password2.length < 8) errorMessage = "Votre mot de passe doit contenir entre 8 et 20 caractères";
+									else if (password != password2) errorMessage = "Les mots de passe ne correspondent pas";
+
+									if (errorMessage) res.writeHead(302, { location: `/inscription?error=${encodeURIComponent(errorMessage)}` }).end();
+									else {
+										const { password: encryptedPassword, passwordSalt } = securePassword(password), userId = randomUUID({ disableEntropyCache: true });
+
+										db.get("SELECT * FROM users WHERE email = ?", email, (err, row) => {
+											if (err) {
+												console.log("Erreur lors de la vérication de l'email: ", err);
+
+												res.writeHead(302, { location: `/inscription?error=${encodeURIComponent("Erreur lors de la vérification de l'email")}` }).end();
+											} else if (row) res.writeHead(302, { location: `/inscription?error=${encodeURIComponent("Cet email est déjà utilisé")}` }).end();
+											else db.exec(`INSERT INTO users (id, email, username, password, password_salt) VALUES (
+												"${userId}",
+												"${email}",
+												"${username}",
+												"${encryptedPassword}",
+												"${passwordSalt}"
+											)`, err => {
+												if (err) {
+													console.log("Erreur lors de la création du compte : ", err);
+
+													res.writeHead(302, { location: `/inscription?error=${encodeURIComponent("Erreur lors de la création du compte ")}` }).end();
+												} else res.writeHead(302, { location: "/", "set-cookie": `token=${randomBytes(64).toString('hex')}.${userId};` }).end();
+											});
+										});
+									};
+								});
 							};
-						});
+							break;
+						default:
+							res.writeHead(404, "Not found").end();
+							break;
 					};
-					break;
-				default:
-					res.writeHead(404, "Not found").end();
-					break;
+				}).listen(8080, () => console.log("http://localhost:8080"));
 			};
-		}).listen(8080, () => console.log("http://localhost:8080"));
-	};
+		}); 
+	});
 });
