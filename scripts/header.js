088355619fc7238713a3eac7a17dd004e0ed8402
Nathan-ecole <nathan94nahtanv2@gmail.com>
2025-03-28 14:52:09 +0100

    Suppression d'un console.log inutile

--- scripts/header.js
+++ scripts/header.js
@@ -2,9 +2,7 @@ const header = document.querySelector("header");
 
 let lastScrollY = window.scrollY;
 
-window.addEventListener("scroll", () => {
-	console.log(window.scrollY);
-	
+window.addEventListener("scroll", () => {	
 	if (lastScrollY < window.scrollY || window.scrollY == 0) header.classList.remove("sticky");
 	else if (window.scrollY > header.clientHeight * 2.5) header.classList.add("sticky");
 
