06cb51a92d1a7faf6adc283b5f6f57c8d3c7d345
Nathan-ecole <nathan94nahtanv2@gmail.com>
2025-04-03 14:52:28 +0200

    Ajout du changement des images dans la page de présentation de chaque produit

---
+++ scripts/produit.js
@@ -0,0 +1,21 @@
+const displayImg = document.getElementById("display"),
+images = document.querySelectorAll("#images-container img");
+
+for (const img of images) {
+	img.addEventListener("click", () => {
+		const currPresentation = document.getElementById("current-presentation"),
+		currPresentationImgLastPartWithoutExtension = `/${currPresentation.src.split("/").at(-1).split(".")[0]}`;
+
+		if (img.id != "current-presentation") {
+			const imgLastPartWithoutExtension = `/${img.src.split("/").at(-1).split(".")[0]}`;
+			
+			console.log(currPresentationImgLastPartWithoutExtension, imgLastPartWithoutExtension);
+			
+			
+			displayImg.src = displayImg.src.replace(currPresentationImgLastPartWithoutExtension, imgLastPartWithoutExtension);
+			displayImg.srcset = displayImg.srcset.replaceAll(currPresentationImgLastPartWithoutExtension, imgLastPartWithoutExtension);
+			img.id = "current-presentation";
+			currPresentation.id = "";
+		};
+	});
+};
